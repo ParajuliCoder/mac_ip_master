45fc4cc6481d7eff6ec076e6db03f4ae69a48e7c
anish <anishparajuli704@gmail.com>
2026-02-04 12:10:08 +0545

    Update mac_ip_changer.sh

--- mac_ip_changer.sh
+++ mac_ip_changer.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root (sudo)."
    exit 1
 fi
 
 INTERFACE="eth0"
+
+print_logo() {
+    echo "=============================================================="
+    echo "  _   _      _                      _ _       _             "
+    echo " | \\ | | ___| |___      _____  _ __| (_) __ _| |_ ___  _ __ "
+    echo " |  \\| |/ _ \\ __\\ \\ /\\ / / _ \\| '__| | |/ _\` | __/ _ \\| '__|"
+    echo " | |\\  |  __/ |_ \\ V  V / (_) | |  | | | (_| | || (_) | |   "
+    echo " |_| \\_|\\___|\\__| \\_/\\_/ \\___/|_|  |_|_|\\__,_|\\__\\___/|_|   "
+    echo "                                                              "
+    echo "                 Created by Anish Parajuli"
+    echo "=============================================================="
+    echo ""
+}
 generate_mac() {
     printf '02:%02x:%02x:%02x:%02x:%02x' $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256))
 }
@@ -13,8 +25,12 @@ generate_mac() {
 generate_ip() {
     echo "192.168.$((RANDOM % 254 + 1)).$((RANDOM % 254 + 1))"
 }
-
-echo "Starting network identity rotation on $INTERFACE..."
+clear
+print_logo
+echo "[*] Targeted Interface: $INTERFACE"
+echo "[*] Rotation Interval: 2 Seconds"
+echo "[*] Press Ctrl+C to stop identity rotation"
+echo "------------------------------------------------------"
 
 while true; do
     NEW_MAC=$(generate_mac)
@@ -25,7 +41,7 @@ while true; do
     ip addr add "$NEW_IP/24" dev "$INTERFACE"
     ip link set "$INTERFACE" up
 
-    echo "[+] Identity changed | MAC: $NEW_MAC | IP: $NEW_IP"
+    echo "[+] $(date +%T) | Identity Shifted -> [MAC: $NEW_MAC] [IP: $NEW_IP]"
 
     sleep 2
 done
